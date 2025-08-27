d466d335bce1035f6c109d812c076417953ef809
Fishybearr <aaronradliff@gmail.com>
2025-08-27 12:27:41 -0400

    Add account creation page and implemented basic image upload system for profile pictures

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
-from os import uname
+import os
 
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, make_response, redirect
 import mariadb
+from werkzeug.utils import secure_filename
+
+UPLOAD_FOLDER = 'uploads/'
+ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg'])
 
 #TODO This are test credentials that will be disabled in production
 db_config = {
@@ -20,13 +24,19 @@ except mariadb.Error as e:
 
 app = Flask(__name__)
 
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
 
 #TODO: Handle HTML escaping
 @app.route('/')
 def hello():
+    cook = request.cookies.get('username')
+    if cook == "testName":
+        return render_template('LoggedInFeed.html')
     return render_template('index.html')
 
 
+#TODO REMOVE THIS
 @app.route('/user/<int:userID>')
 def displayUserHome(userID):
     return f"some user home {userID}"
@@ -35,6 +45,12 @@ def displayUserHome(userID):
 """Displays and processes login form"""
 @app.route('/login', methods=['GET', 'POST'])
 def loginPage():
+    cook = request.cookies.get('username')
+    if cook == "testName":
+        return '<meta http-equiv="refresh" content="0; url=http://127.0.0.1:5000/">" />'
+
+
+
     if request.method == 'POST':
         uname = request.form.get('uname')
         psswd = request.form.get('psswd')
@@ -45,17 +61,58 @@ def loginPage():
 
         row = cursor.fetchone()
 
-        #currently only fetching the first row of the db
-        #in the future we search the db for the username and then
-        #validate the password
-        if row != None and row[1] == uname and row[2] == psswd:
-            return f"{uname},{psswd},{row}"
+        #Check if the username is in the DB and then validate
+        #password hash (hashing not implemented yet)
+        #TODO: Implement hashing function here
+        if row != None and row[2] == psswd:
+            resp = make_response('<meta http-equiv="refresh" content="0; url=http://127.0.0.1:5000/">" />')
+            resp.set_cookie('username', 'testName', secure=True, max_age=3600) #TODO: enable httponly and esnure that js can stil send the cookie
+            return resp
+
+            #Eventually will send a cookie here and store it
+            #in the DB, then give access to custom feed page
         else:
             return f"invalid login"
         cursor.close()
 
     return render_template('login.html')
 
+
+#TODO: Secure the file upload and info validation systems further
+@app.route('/createAccount', methods=['GET', 'POST'])
+def createAccount():
+    if request.method == 'POST':
+        #process form image
+        if 'profilePic' not in request.files:
+            return redirect(request.url)
+
+        profilePic = request.files['profilePic']
+
+        if profilePic.filename == '':
+            return redirect(request.url)
+
+        fn = secure_filename(profilePic.filename)
+        profilePic.save(os.path.join(app.config['UPLOAD_FOLDER'], fn))
+        #save this filepath to the DB with the user
+
+        #process from text
+        uname = request.form.get('uname')
+        psswd = request.form.get('psswd')
+        cat_name = request.form.get('cat_name')
+        return f"<p> catname: {cat_name}\n username: {uname} password: {psswd}</p>"
+
+    return render_template('CreateAccount.html')
+
+
+"""
+@app.route('/random', methods = ['GET', 'POST'])
+def random():
+
+    #This will set the cookie for the user and then redirect them to the home page
+    resp = make_response('<meta http-equiv="refresh" content="0; url=http://127.0.0.1:5000/">" />')
+    resp.set_cookie('username','testName')
+    return resp
+"""
 
 if __name__ == '__main__':
     app.run(debug=True)
